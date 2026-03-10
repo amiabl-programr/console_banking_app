05aa56ddae85104f426e34871dfb14d9ae857afa
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-03-10 17:04:29 +0100

    create a separate account class

---
+++ ConsoleApp1/Account.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleBankingApp
+{
+    public class Account
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+        public string password;
+        public decimal Balance;
+
+       
+    }
+}
