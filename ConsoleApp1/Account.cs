9af8be22f25b55d322d02a8092db780dc0e51690
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-03-10 18:21:03 +0100

    add constructor and override string

--- ConsoleApp1/Account.cs
+++ ConsoleApp1/Account.cs
@@ -8,8 +8,21 @@ namespace ConsoleBankingApp
         public string lastName;
         public string email;
         public string password;
-        public decimal Balance;
+        public decimal Balance;  
 
-       
+        public Account (string firstname, string lastName, string email, string password)
+        {
+            this.firstName = firstname;
+            this.lastName = lastName;
+            this.email = email;
+            this.password = password;
+            this.Balance = 0;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {firstName} {lastName}, Email: {email}, Balance: {Balance}";
+        }
     }
 }
