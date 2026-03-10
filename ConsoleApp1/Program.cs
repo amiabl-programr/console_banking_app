7b7c29a622a78c2f87b0840ddeb054c8deb87e09
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2026-03-10 14:48:01 +0100

    add accounts class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,15 +7,24 @@ namespace ConsoleBankingApp
     class Bank
     {
         static bool WelcomeToApp = true;
-       
+        static List<Accounts> accounts = new List<Accounts>();
+        class Accounts
+        {
+            public string firstName;
+            public string lastName;
+            public string email;
+            public string password;
+            public decimal Balance;
+
+        }
         static void Main(string[] args)
         {
-             
-       
+
+
             while (WelcomeToApp)
             {
-            Console.WriteLine("Select an option: ");
-            int selectedOption = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Select an option: ");
+                int selectedOption = Convert.ToInt32(Console.ReadLine());
 
                 if (selectedOption == 1)
                 {
@@ -27,8 +36,8 @@ namespace ConsoleBankingApp
                     // run Login
                     Login();
                 }
-                else if (selectedOption == 3) 
-                { 
+                else if (selectedOption == 3)
+                {
                     // Logout
                     WelcomeToApp = false;
                 }
@@ -54,14 +63,43 @@ namespace ConsoleBankingApp
             Console.WriteLine("Welcome to the Register page!");
             Console.WriteLine("==========================");
 
-            
+            Console.WriteLine("1. Fristname: ");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("2. Lastname: ");
+            string lastname = Console.ReadLine();
+
+            Console.WriteLine("3. Email: ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("4. Email: ");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("5. Initial Deposit: ");
+            decimal initialDeposit = Convert.ToDecimal(Console.ReadLine());
+
+            Accounts newUser = new Accounts
+            {
+                firstName = firstname,
+                lastName = lastname,
+                email = email,
+                password = password,
+                Balance = initialDeposit
+            };
+
+            accounts.Add(newUser);
+            Console.WriteLine("Congratulations! ");
         }
 
         static void Login()
         {
             Console.WriteLine("Welcome to the Login page!");
 
-  
+            Console.WriteLine("Enter Email: ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Enter password: ");
+            string password = Console.ReadLine();
 
 
         }
